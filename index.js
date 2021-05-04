f4f311899fe738b569425a35991987e81c05a9f6
AibersonDev <aiberson.dev@gmail.com>
2021-05-04 19:50:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Client, Collection } = require("discord.js");
+const client = new Client();
+const fs = require("fs");
+const config = require("./config.json");
+
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+let prefix = config.prefix;
+
+client.on('ready', () => {
+    console.log('Ready');
+
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: 'hestibots.xyz',
+            type: 'PLAYING'
+        }
+    });
+});
+
+client.on('message', async (message) => {
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName) || client.commands.find((cmd) => cmd.alias && cmd.alias.includes(commandName));
+
+    command.run(client, message, args);
+});
+
+client.login(config.token);
